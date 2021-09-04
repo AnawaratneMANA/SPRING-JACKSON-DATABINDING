d88aae65ab918bb30ca40a89b06ffa786d6e5b7a
AnawaratneMANA <it19086408@my.sliit.lk>
2021-09-04 07:33:15 -0400

    Inital Empty Template Added

---
+++ src/test/java/lk/jackson/databinding/DatabindingApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.jackson.databinding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabindingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
